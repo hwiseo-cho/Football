0f3ed1d079e34401266ac8b5b5149ef0e65f748b
hwiseo <gnltj37@naver.com>
2023-11-24 11:47:06 +0900

    test

--- FOOTBALL/src/main/java/com/football/ftb/web/FtbController.java
+++ FOOTBALL/src/main/java/com/football/ftb/web/FtbController.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.slf4j.LoggerFactory; 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
